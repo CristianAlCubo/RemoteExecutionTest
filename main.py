b4f6327b6108f6168ea00c9d237f56db8cb48a71
Camilo Cubillos <4282923-CamiloCubillos1@users.noreply.replit.com>
2024-03-22 15:58:16 +0000

    Changes

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 print("Hola mundo desde GitHub!")
+print("------------------------------")
 for i in range(10):
   print(i)
+print("------------------------------")
+
+def sayHello():
+  print("Esto es un print desde una función!!!")
+sayHello()
