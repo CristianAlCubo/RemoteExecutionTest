e372cf97ed5285d7dc47095e8be008cfc3defa9d
Camilo Cubillos <4282923-CamiloCubillos1@users.noreply.replit.com>
2024-04-05 15:06:50 +0000

    changes

--- main3.py
+++ main3.py
@@ -1,2 +1,15 @@
+import csv
+
+def readCSV():
+  ruta_archivo = 'data/keys.csv'
+
+  print("Leyendo archivo CSV...")
+  with open(ruta_archivo, newline='') as archivo_csv:
+      lector_csv = csv.reader(archivo_csv)
+      # Itera sobre cada fila del archivo CSV
+      for fila in lector_csv:
+          print(fila)
+
 def holaDesdeMain3():
   print("Hola mundo desde main 3")
+  readCSV()
